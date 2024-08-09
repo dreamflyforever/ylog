c6a83b08ef4ee1c14e1df8261541f8924af62c5d
dreamflyforever <1150660744@qq.com>
2024-08-09 14:43:24 +0800

    save log in flash
    support time log
    support format parameter
    
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- core.c
+++ core.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-int now()
+char time_str[80];
+char * ylog_get_time()
 {
     time_t rawtime;
     struct tm *timeinfo;
@@ -12,25 +14,71 @@ int now()
     time(&rawtime);
     timeinfo = localtime(&rawtime);
 
-    char time_str[80];
     strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", timeinfo);
-    printf("%s", time_str);
-
-    return 0;
+    return time_str;
 }
-#define DEBUG 1
+
+int ylog_now_print()
+{
+	printf("%s", ylog_get_time());
+	return 0;
+}
+
+#define YLOG_FILE 1
+
+#if YLOG_FILE
+int g_fd;
+int ylog_file_init()
+{
+	char tmp_file[94];
+	snprintf(tmp_file, 94, "%s", ylog_get_time());
+	g_fd = open(tmp_file, O_CREAT | O_RDWR, 0666);
+	if (g_fd == -1) {
+		perror("error open file\n");
+		return 1;
+	} else {
+		printf("open %s success\n", tmp_file);
+	}
+	return 0;
+}
+
+int ylog_file_close()
+{
+	close(g_fd);
+	return 0;
+}
+#endif
+
+#define DEBUG 0
 #if DEBUG
 #define ylog(format, ...) \
-	{now(); printf(" [%s : %s : %d] ", \
+	{ylog_now_print(); printf(" [%s : %s : %d] ", \
 	__FILE__, __func__, __LINE__); \
 	printf(format, ##__VA_ARGS__);}
+#elif YLOG_FILE
+
+#define ylog(format, ...) \
+{ \
+	char tmp[512] = {0}; \
+	snprintf(tmp, 128, "%s [%s : %s : %d] ", ylog_get_time(),  __FILE__, __func__, __LINE__); \
+	write(g_fd, tmp, strlen(tmp));\
+	memset(tmp, 0, 512); \
+	snprintf(tmp, 128, format, ##__VA_ARGS__); \
+	write(g_fd, tmp, strlen(tmp)); }
+
 #else
-#define os_printf(format, ...) 
+#define ylog(format, ...) 
 #endif
 
 int main(int argc, char *argv[])
 {
+#if YLOG_FILE
+	ylog_file_init();
+#endif
 	ylog("hello\n");
 	ylog("world\n");
+#if YLOG_FILE
+	ylog_file_close();
+#endif
 	return 0;
 }
